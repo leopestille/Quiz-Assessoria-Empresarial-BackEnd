f5efe1b9c25b2b5a7a1892dd0d8c031f8003ef6e
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-07-17 14:11:44 -0400

    fix: consertando controller

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -4,8 +4,41 @@ import { checkPassword } from "../services/Auth";
 import authConfig from "../config/auth";
 
 class SessionController {
-    async create(req, res) {
-        return res.status(200).json(req.user);
+    async create(requisition, response) {
+        const { email, password } = requisition.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            response.status(401).json({ message: "Usuário ou Senha Incorretos" });
+            return;
+        }
+
+        if (!checkPassword(user, password)) {
+            response.status(401).json({ message: "Usuário ou Senha Incorretos" });
+            return;
+        }
+
+        const { id, name } = user;
+
+        user.sessionCount = (user.sessionCount || 0) + 1;
+
+        await user.save();
+
+        if (user.sessionCount > 3) {
+            response.status(401).json({ message: "Limite de sessões excedido" });
+            return;
+        }
+        return response.status(200).json({
+            user: {
+                id,
+                name,
+                email
+            },
+            token: jwt.sign({ id }, authConfig.secret, {
+                expiresIn: authConfig.expiresIn,
+            })
+        });
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,13 @@ import { Router } from "express";
 import UsersController from "./controllers/UsersController";
 import auth from "./middlewares/auth";
 import SessionsController from "./controllers/SessionsController";
-import verifySession from "./middlewares/verifySession";
 
 const routes = new Router();
 
-routes.post("/sessions", verifySession, SessionsController.create);
+routes.post("/sessions", SessionsController.create);
 routes.post("/users", UsersController.create);
 
-//routes.use(auth);
+routes.use(auth);
 
 routes.get("/users", UsersController.index);
 routes.delete("/users/:id", UsersController.destroy);
