b9758f166beea82ac87522508505a3ab5236870e
Leonardo Pestille <57184780+leopestille@users.noreply.github.com>
2023-08-01 10:22:49 -0400

    criando disparo de email de redefinicao

--- src/routes.mjs
+++ src/routes.mjs
@@ -3,10 +3,12 @@ import UsersController from "./controllers/UsersController.mjs";
 import auth from "./middlewares/auth.mjs";
 import SessionsController from "./controllers/SessionsController.mjs";
 import crypto from "node:crypto";
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
 import User from "./models/User.mjs";
 
 const routes = new Router();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 routes.get("/", (req, res) => {
     res.send("API up");
@@ -15,53 +17,53 @@ routes.get("/", (req, res) => {
 routes.post("/sessions", SessionsController.create);
 routes.post("/users", UsersController.create);
 
-routes.post("/forgot-password", function(req, res) {
-    crypto.randomBytes(20, function(err, buf) {
-        const token = buf.toString("hex");
+routes.post("/forgot-password", async function (req, res) {
+  
+  crypto.randomBytes(20, async function (err, buf) {
+    
+    const token = buf.toString("hex");
 
-        User.findOne({ email: req.body.email }, function(err, user) {
-            if (!user) {
-                return res
-                  .status(404)
-                  .send(
-                    "Não existe nenhuma conta com esse endereço de e-mail."
-                  );
-            }
-            user.passwordResetToken = token;
-            user.passwordResetExpires = Date.now() + 3600 * 1000;
+    try {
+      const user = await User.findOne({ email: req.body.email });
 
-            user.save().then(() => {
-                const smtpTransport = nodemailer.createTransport({
-                    service: "Gmail",
-                    auth: {
-                        user: "leonardopestille@gmail.com",
-                        pass: "Rhauane@103",                        
-                    },
-                });
-            const mailOptions = {
-              to: user.email,
-              from: "leonardopestille@hotmail.com",
-              subject: "Reset de Senha do QUIZ RELATÓRIO DE NEGÓCIO",
-              text:
-                "Você está recebendo esta mensagem porque você (ou outra pessoa) solicitou a redefinição de senha para sua conta.\n\n" +
-                "Por favor, clique no seguinte link ou copie e cole-o em seu navegador para completar o processo:\n\n" +
-                "http://" +
-                req.headers.host +
-                "/reset/" +
-                token +
-                "\n\n" +
-                "Caso você não tenha feito essa solicitação, por favor, ignore este e-mail e sua senha permanecerá inalterada.\n",
-            };
+      if (!user) {
+        return res
+          .status(404)
+          .send("Não existe nenhuma conta com esse endereço de e-mail.");
+      }
 
-            smtpTransport.sendMail(mailOptions, function(err) {
-                res.send(
-                  "Um email foi enviado para ' + user.email + ' com mais instruções."
-                );
-            })
-            })
+      const msg = {
+        to: user.email,
+        from: "contato@datainsight.com.br",
+        subject: "Reset de Senha do QUIZ RELATÓRIO DE NEGÓCIO",
+        text:
+          "Você está recebendo esta mensagem porque você (ou outra pessoa) solicitou a redefinição de senha para sua conta.\n\n" +
+          "Por favor, clique no seguinte link ou copie e cole-o em seu navegador para completar o processo:\n\n" +
+          "http://" +
+          req.headers.host +
+          "/reset/" +
+          token +
+          "\n\n" +
+          "Caso você não tenha feito essa solicitação, por favor, ignore este e-mail e sua senha permanecerá inalterada.\n",
+      };
+
+      sgMail
+        .send(msg)
+        .then(() => {
+          res.send(
+            "Um email foi enviado para " + user.email + " com mais instruções."
+          );
         })
-    })
-})
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send("Erro ao enviar o e-mail.");
+        });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Ocorreu um erro ao procurar o usuário.");
+    }
+  });
+});
 
 routes.use(auth);
 
